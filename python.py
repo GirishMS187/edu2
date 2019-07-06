c804a3ddb51352bc072cb48688283ae72f264883
girish <girish.shet187@gmail.com>
2019-07-06 08:08:28 +0530

    merge

--- python.py
+++ python.py
@@ -5,10 +5,10 @@ print("Result of additon is:", a+b)
 print("value of a is:",a)
 print("Value of b is:",b)
 
-print("Value of a and b are:", a,b)
+print("Value of a and b are:", a,b, "there's merge conflit need to be created")
 print("Value of a+b is:",a+b)
 
 c=10
-d=10
+d=20
 
 print("c and d")
